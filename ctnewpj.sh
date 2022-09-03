d714cd7951fa7e87299ae1a7bbb32fa38009a7af
Gabriel VS Frasão <gabrielvitor.frasao@gmail.com>
2022-09-03 11:07:46 -0300

    test(ctnewpj.sh): test default action

--- ctnewpj.sh
+++ ctnewpj.sh
@@ -113,20 +113,13 @@ prompt_for_multiselect result "Typescript;Personal sugestion;Testing (based on V
 # ! [0] => Typescript; [1] => Personal sugestion; [2] => Testing (based on Vitest); [3] => Animations; [4] => PWA support;
 
 # TODO ASK Y/N QUESTIONS (next-pwa options, unit and e2e?, create repo?, GitHub CLI?, commitizen for semantic versioning?)...
-# while true; do
-#     read -p "Do you wish to create a github repo? [yn] " yn
-#     case $yn in
-#         [Yy]* ) createRepo=true; break;;
-#         [Nn]* ) createRepo=false; exit;;
-#         * ) echo "Please answer yes or no.";;
-#     esac
-# done
-
-echo "Do you wish to create a GitHub repository?"
-select yn in "Yes" "No"; do
+while true; do
+    read -p "Do you wish to create a github repo? [yn] " yn
     case $yn in
-        Yes ) createRepo=true; break;;
-        No ) createRepo=false; exit;;
+        [Yy]* ) createRepo=true; break;;
+        [Nn]* ) createRepo=false; exit;;
+        []* ) echo 'no answer, so use this default'; break;;
+        * ) echo "Please answer yes or no.";;
     esac
 done
 
